bda239e9557dd5bc21c1a0d24d17de9e0a392616
JesseDev3 <jesse.d.everington@gmail.com>
2025-04-17 20:04:49 -0400

    Update el9_setup.sh

--- el9_setup.sh
+++ el9_setup.sh
@@ -9,6 +9,19 @@ gprofng
 drgn
 corelens
 
+# Node.js
+# Download and install nvm:
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.2/install.sh | bash
+# in lieu of restarting the shell
+\. "$HOME/.nvm/nvm.sh"
+# Download and install Node.js:
+nvm install 22
+# Verify the Node.js version:
+node -v # Should print "v22.14.0".
+nvm current # Should print "v22.14.0".
+# Verify npm version:
+npm -v # Should print "10.9.2".
+
 # Java
 sudo yum install -y nodejs npm java-17-openjdk java-17-openjdk-devel 
 # sudo alternatives --config java 
